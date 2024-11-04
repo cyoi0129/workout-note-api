1391d1d700ccfbebf6bc140b5ff582ce2fa1b9da
cyoi0129 <cyoi0129@gmail.com>
2024-11-05 08:54:33 +0900

    add ssl mode

--- go/models/db.go
+++ go/models/db.go
@@ -18,12 +18,12 @@ func ConnectDatabase() {
 	dbUser := os.Getenv("DATABASE_USER")
 	dbHost := os.Getenv("DATABASE_HOST")
 	dbPassword := os.Getenv("DATABASE_PASSWORD")
+	sslmode := os.Getenv("SSL_MODE")
+	fmt.Println(sslmode)
+
 	dbPort := 5432
-	// dsn := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
-	// 	dbHost, dbPort, dbUser, dbPassword, dbName,
-	// )
-	dsn := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s",
-		dbHost, dbPort, dbUser, dbPassword, dbName,
+	dsn := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=%s",
+		dbHost, dbPort, dbUser, dbPassword, dbName, sslmode,
 	)
 
 	db, err := sql.Open("postgres", dsn)
